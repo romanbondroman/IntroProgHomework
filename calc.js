bba3d86beb86efcf3bdfa63f1c20bad332123304
Roman Bond <roman.bond1@marist.edu>
2014-11-07 08:11:58 -0500

    Create calc.js

---
+++ calc.js
@@ -0,0 +1,17 @@
+//Triangle
+
+var run = function() {
+var inputBox = document.getElementById('scores');
+-	var inputStr = inputBox.value;
+	var base=1;
+        var i = 2       
+        for(x=1; x<n; x++){
+                base = base + i;
+                i++;
+        }
+        return base;
+};
+
+var button = document.getElementById('runner');
+button.onclick = run;
+
